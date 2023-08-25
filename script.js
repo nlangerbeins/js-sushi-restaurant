245a2bdec346c83749183b0a15c6dac05929d3f8
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-08-25 21:35:40 +0200

    change footer year

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 /*--------Header + title-----------*/
-gsap.to ('.nav li', {duration: 1, y: -20, stagger: 0.3, opacity: 1, delay: 0.5})
-gsap.to ('.logo', {duration: 1, y: -20, opacity: 1, delay: 0.5})
-gsap.to ('.logo', {duration: 1, y: -20, opacity: 1, delay: 0.5})
-gsap.to ('.logo', {duration: 1, y: -20, opacity: 1, delay: 0.5})
-gsap.to ('.heading h2', {duration: 1, y:  -20, opacity: 1, delay: 1.5})
-gsap.to ('.heading p', {duration: 1, y:  -30, opacity: 1, delay: 2})
-gsap.to ('.fusion', {x: 300, delay: 2.7, duration: 2, opacity: 1, })
-gsap.to ('.sushi', {x: -300, delay: 2.7, duration: 2, opacity: 1, })
-gsap.to ('.title_img__nigiri', {rotation: 720, opacity: 1, delay: 3.6, duration: 2})
-gsap.to ('.title_img__bg', {opacity: 1, delay: 4, duration: 1.5})
+gsap.to('.nav li', {
+  duration: 1,
+  y: -20,
+  stagger: 0.3,
+  opacity: 1,
+  delay: 0.5,
+});
+gsap.to('.logo', { duration: 1, y: -20, opacity: 1, delay: 0.5 });
+gsap.to('.logo', { duration: 1, y: -20, opacity: 1, delay: 0.5 });
+gsap.to('.logo', { duration: 1, y: -20, opacity: 1, delay: 0.5 });
+gsap.to('.heading h2', { duration: 1, y: -20, opacity: 1, delay: 1.5 });
+gsap.to('.heading p', { duration: 1, y: -30, opacity: 1, delay: 2 });
+gsap.to('.fusion', { x: 300, delay: 2.7, duration: 2, opacity: 1 });
+gsap.to('.sushi', { x: -300, delay: 2.7, duration: 2, opacity: 1 });
+gsap.to('.title_img__nigiri', {
+  rotation: 720,
+  opacity: 1,
+  delay: 3.6,
+  duration: 2,
+});
+gsap.to('.title_img__bg', { opacity: 1, delay: 4, duration: 1.5 });
 
-
-/*---------Hamburger menu--------------*/
+// Hamburger menu
 let navs = document.querySelectorAll('#navItem');
 
-navs.forEach(nav => {
-    nav.addEventListener ('click', () => {
-        document.querySelector('#menu_toggle').checked = false;
-    })
-})
+navs.forEach((nav) => {
+  nav.addEventListener('click', () => {
+    document.querySelector('#menu_toggle').checked = false;
+  });
+});
+
+// Footer
+const currentYear = new Date().getFullYear();
+const footer = document.querySelector('footer');
+footer.querySelector(
+  'p'
+).innerHTML = `© ${currentYear} by <span>Natallia Langerbeins</span>`;
